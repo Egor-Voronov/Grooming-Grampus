43391cd1ccc9fffd64ae119e67ce301ae4f62de3
javacripteaser88 <vip.egor.voronov.2016@mail.ru>
2022-10-26 00:58:02 +0300

    day6 commit

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,26 +2,26 @@
 import '../style/style.scss'
 
 // логика поведения навигации
-const body = document.querySelector("body");
-const navbar = document.querySelector(".navbar");
-const menu = document.querySelector(".menu-list");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
+const body = document.querySelector('body');
+const navbar = document.querySelector('.navbar');
+const menu = document.querySelector('.menu-list');
+const menuBtn = document.querySelector('.menu-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
 menuBtn.onclick = () => {
-    menu.classList.add("active");
-    menuBtn.classList.add("hide");
-    cancelBtn.classList.add("show");
-    body.classList.add("disabledScroll");
+    menu.classList.add('active');
+    menuBtn.classList.add('hide');
+    cancelBtn.classList.add('show');
+    body.classList.add('disabledScroll');
 }
 cancelBtn.onclick = () => {
-    menu.classList.remove("active");
-    menuBtn.classList.remove("hide");
-    cancelBtn.classList.remove("show");
-    body.classList.remove("disabledScroll");
+    menu.classList.remove('active');
+    menuBtn.classList.remove('hide');
+    cancelBtn.classList.remove('show');
+    body.classList.remove('disabledScroll');
 }
 
 window.onscroll = () => {
-    window.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    window.scrollY > 20 ? navbar.classList.add('sticky') : navbar.classList.remove('sticky');
 }
 
 //параллакс
@@ -36,26 +36,45 @@ new Swiper('.banner', {
     },
     speed: 1000,
     slideToClickedSlide: false,
-})
+});
+
+// плавный скролл
+const anchors = document.querySelectorAll('.nav-li[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const blockID = anchor.getAttribute('href').substr(1);
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+};
+
+// защитная мера, чтобы возможные следующие соискатели, не брали мою работу)
+console.log("Данное тестовое задание выполнил Воронов Егор")
 
 // уменьшаю шрифт
 // /* Функция пересчёта размера шрифта */
 // function fGummaFontSize() {
 //     /* Увеличиваем размер шрифта, до появления прокрутки */
 //     while (this.scrollHeight <= this.clientHeight || this.scrollWidth <= this.clientWidth) {
-//       this.style.fontSize = parseFloat(getComputedStyle(this).fontSize) + 2 + "px";
+//       this.style.fontSize = parseFloat(getComputedStyle(this).fontSize) + 2 + 'px';
 //     }
 //     /* Уменьшаем размер шрифта, пока прокрутка не исчезнет */
 //     while ( this.scrollHeight > this.clientHeight || this.scrollWidth > this.clientWidth ) {
-//       this.style.fontSize = parseFloat(getComputedStyle(this).fontSize) - 1 + "px";
+//       this.style.fontSize = parseFloat(getComputedStyle(this).fontSize) - 1 + 'px';
 //     }
 //   }
   
 //   /* Функция обхода всех элементов с нужным классом */
 //   function fGummaResizeAll() {
-//     document.querySelectorAll(".gumma").forEach(el => fGummaFontSize.call(el));
+//     document.querySelectorAll('.gumma').forEach(el => fGummaFontSize.call(el));
 //   }
   
-//   document.querySelectorAll(".gumma").forEach(el => el.addEventListener("input", fGummaFontSize));
+//   document.querySelectorAll('.gumma').forEach(el => el.addEventListener('input', fGummaFontSize));
 //   window.onload = fGummaResizeAll; // Запуск после загрузки контента
 //   window.onresize = fGummaResizeAll; // Запуск при изменении размеров окна и контейнеров
